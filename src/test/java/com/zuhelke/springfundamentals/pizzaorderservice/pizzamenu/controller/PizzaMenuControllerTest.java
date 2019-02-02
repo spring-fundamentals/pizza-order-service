c28d6825386d8267a09d549c56df05022a3607b7
Marius Reusch <marius.reusch@gmail.com>
2019-02-02 23:33:26 +0100

    add one test with jacksontester

--- src/test/java/com/zuhelke/springfundamentals/pizzaorderservice/pizzamenu/controller/PizzaMenuControllerTest.java
+++ src/test/java/com/zuhelke/springfundamentals/pizzaorderservice/pizzamenu/controller/PizzaMenuControllerTest.java
@@ -5,12 +5,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zuhelke.springfundamentals.pizzaorderservice.pizzamenu.service.PizzaMenuService;
 import java.util.Optional;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,12 +28,18 @@ public class PizzaMenuControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  private JacksonTester<PizzaMenuItemDto> json;
+
+  @Before
+  public void setup() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JacksonTester.initFields(this, objectMapper);
+  }
   @Test
   public void unknownId_getById_notFoundStatus() throws Exception {
     String unknownId = "unknownId";
 
     mockMvc.perform(get("/pizzas/" + unknownId))
-
         .andExpect(status().isNotFound())
         .andExpect(content().json("{\"title\":\"Not Found\",\"status\":404,\"detail\":\"The requested resource is not available\"}"));
   }
@@ -44,5 +53,16 @@ public class PizzaMenuControllerTest {
 
         .andExpect(status().isOk())
         .andExpect(content().json("{\"name\":\"Pizza Funghi\",\"price\":13.0}"));
+  }
+
+  @Test
+  public void knownId_getById_correctObject() throws Exception {
+    String knownId = "100";
+    doReturn(Optional.of(new PizzaMenuItemDto(knownId, "Pizza Funghi", 13))).when(pizzaMenuService).findById(knownId);
+
+    mockMvc.perform(get("/pizzas/" + knownId))
+
+        .andExpect(status().isOk())
+        .andExpect(content().json(json.write(new PizzaMenuItemDto(knownId, "Pizza Funghi", 13)).getJson()));
   }
 }
