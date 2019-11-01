79eda234f91f743808c99ff3ca7c03ad8f14fc01
Sebastian Zillessen <sebastian@zillessen.info>
2019-11-01 16:27:22 +0100

    Reformat code

--- src/test/java/com/zuhelke/springfundamentals/pizzaorderservice/pizzamenu/controller/PizzaMenuControllerTest.java
+++ src/test/java/com/zuhelke/springfundamentals/pizzaorderservice/pizzamenu/controller/PizzaMenuControllerTest.java
@@ -1,9 +1,5 @@
 package com.zuhelke.springfundamentals.pizzaorderservice.pizzamenu.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,48 +11,53 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PizzaMenuControllerTest {
 
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  private JacksonTester<PizzaMenuItemDto> json;
+    private JacksonTester<PizzaMenuItemDto> json;
 
-  @Before
-  public void setup() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    JacksonTester.initFields(this, objectMapper);
-  }
-  @Test
-  public void unknownId_getById_notFoundStatus() throws Exception {
-    String unknownId = "unknownId";
+    @Before
+    public void setup() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JacksonTester.initFields(this, objectMapper);
+    }
 
-    mockMvc.perform(get("/pizzas/" + unknownId))
-        .andExpect(status().isNotFound())
-        .andExpect(content().json("{\"title\":\"Not Found\",\"status\":404,\"detail\":\"The requested resource is not available\"}"));
-  }
+    @Test
+    public void unknownId_getById_notFoundStatus() throws Exception {
+        String unknownId = "unknownId";
 
-  @Test
-  public void knownId_getById_okStatus() throws Exception {
-    String knownId = "2";
+        mockMvc.perform(get("/pizzas/" + unknownId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().json("{\"title\":\"Not Found\",\"status\":404,\"detail\":\"The requested resource is not available\"}"));
+    }
 
-    mockMvc.perform(get("/pizzas/{id}", knownId))
+    @Test
+    public void knownId_getById_okStatus() throws Exception {
+        String knownId = "2";
 
-        .andExpect(status().isOk())
-        .andExpect(content().json("{\"id\": \"2\",  \"name\": \"Pizza Salami\", \"price\": 18.0}", true));
-  }
+        mockMvc.perform(get("/pizzas/{id}", knownId))
 
-  @Test
-  public void knownId_getById_correctObject() throws Exception {
-    String knownId = "2";
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\": \"2\",  \"name\": \"Pizza Salami\", \"price\": 18.0}", true));
+    }
 
-    mockMvc.perform(get("/pizzas/{id}", knownId))
+    @Test
+    public void knownId_getById_correctObject() throws Exception {
+        String knownId = "2";
 
-        .andExpect(status().isOk())
-        .andExpect(content().json(json.write(new PizzaMenuItemDto(knownId, "Pizza Salami", 18)).getJson()));
-  }
+        mockMvc.perform(get("/pizzas/{id}", knownId))
+
+                .andExpect(status().isOk())
+                .andExpect(content().json(json.write(new PizzaMenuItemDto(knownId, "Pizza Salami", 18)).getJson()));
+    }
 }
