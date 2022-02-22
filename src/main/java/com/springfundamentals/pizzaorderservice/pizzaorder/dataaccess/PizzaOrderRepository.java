eee0d190c52d08e4dd9f63b4b721bf155da5265f
Marius Reusch <marius.reusch1@swisscom.com>
2022-02-22 20:53:29 +0100

    add log statement

--- src/main/java/com/springfundamentals/pizzaorderservice/pizzaorder/dataaccess/PizzaOrderRepository.java
+++ src/main/java/com/springfundamentals/pizzaorderservice/pizzaorder/dataaccess/PizzaOrderRepository.java
@@ -4,10 +4,15 @@ import com.springfundamentals.pizzaorderservice.pizzaorder.domain.PizzaOrder;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PizzaOrderRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(PizzaOrderRepository.class);
 
     public List<PizzaOrder> findAll() {
         return InMemoryDB.PIZZA_ORDERS;
@@ -15,6 +20,7 @@ public class PizzaOrderRepository {
 
     public PizzaOrder save(PizzaOrder pizzaOrder) {
         InMemoryDB.PIZZA_ORDERS.add(pizzaOrder);
+        logger.info("Pizza saved");
         return pizzaOrder;
     }
 
